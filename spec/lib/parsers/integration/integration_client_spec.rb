7e0b48d5da1ffe716d87e52f6745dca633eda823
Paulo Rodrigues <paulo.rodrigues@rebase.com.br>
2021-05-23 21:18:38 -0300

    structure

--- spec/lib/parsers/integration/integration_client_spec.rb
+++ spec/lib/parsers/integration/integration_client_spec.rb
@@ -2,7 +2,4 @@ describe IntegrationClient do
   let(:response) { IntegrationClient.new(url: 'https://friends-quotes-api.herokuapp.com/quotes') }
 
   it 'return a response object', :vcr do
-    expect(response.call.first).to include(:character=>"Ross")
-  end
-end
 
